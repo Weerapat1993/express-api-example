14ce34902948101d87be40d2782cbdc51fca1609
Top <weerapat.thawatchoketawee@gmail.com>
2018-02-17 22:00:43 +0700

    Update Message API Docs

--- app/controllers/PurchaseController.js
+++ app/controllers/PurchaseController.js
@@ -2,21 +2,7 @@ import { Purchase } from '../models';
 import { APIExpection, SuccessCase } from '../tools/RestfulAPI';
 
 /**
- * @api {get} /purchases GET Purchase Lists
- * @apiSampleRequest /api/purchases
- * @apiName GetPurchases
- * @apiGroup Purchase
- *
- * @apiSuccess {String} firstname Firstname of the User.
- * @apiSuccess {String} lastname  Lastname of the User.
- *
- * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *       "firstname": "John",
- *       "lastname": "Doe"
- *     }
- *
+ * @apiDefine ErrorResponse
  * @apiError UserNotFound The <code>id</code> of the User was not found.
  *
  * @apiErrorExample Error-Response:
@@ -26,6 +12,34 @@ import { APIExpection, SuccessCase } from '../tools/RestfulAPI';
  *       "code": 404,
  *       "status": "Not Found",
  *     }
+ */
+
+/**
+ * @api {get} /purchases GET Purchase Lists
+ * @apiSampleRequest /api/purchases
+ * @apiName GetPurchases
+ * @apiGroup Purchase
+ *
+ * @apiSuccess {Array} data  Data Purchase List Response
+ * @apiSuccess {Number} code  Status Code
+ * @apiSuccess {String} status  Status Message
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "data": [
+ *             {
+ *                 "purchase_id": "purchase:1",
+ *                 "products": "product:1",
+ *                 "shop_id": "shop:1",
+ *                 "created_at": "2018-02-17T06:02:23.000Z",
+ *                 "updated_at": "2018-02-17T06:02:23.000Z"
+ *             }
+ *         ],
+ *         "code": 200,
+ *         "status": "Success"
+ *     }
+ * @apiUse ErrorResponse
  */
 
 /**
@@ -47,25 +61,34 @@ export const getAllPurchase = (req, res) => {
  * @apiName GetPurchasesByID
  * @apiGroup Purchase
  *
- * @apiSuccess {String} firstname Firstname of the User.
- * @apiSuccess {String} lastname  Lastname of the User.
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *       "id": "purchase:1"
+ *     }
+ * @apiHeaderExample {json} Header-Example:
+ *     {
+ *       "Authentication": "{{token}}"
+ *     }
  *
- * @apiSuccessExample Success-Response:
+ * @apiSuccess {Object} data  Data Purchase Response
+ * @apiSuccess {Number} code  Status Code
+ * @apiSuccess {String} status  Status Message
+ *
+ * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
  *     {
- *       "firstname": "John",
- *       "lastname": "Doe"
+ *       "data": {
+ *         "purchase_id": "purchase:1",
+ *         "products": "product:1",
+ *         "shop_id": "shop:1",
+ *         "created_at": "2018-02-17T06:02:23.000Z",
+ *         "updated_at": "2018-02-17T06:02:23.000Z"
+ *       },
+ *       "code": 200,
+ *       "status": "Success"
  *     }
  *
- * @apiError UserNotFound The <code>id</code> of the User was not found.
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 404 Not Found
- *     {
- *       "error": "Purchase is not found.",
- *       "code": 404,
- *       "status": "Not Found",
- *     }
+ * @apiUse ErrorResponse
  */
 
 /**
