0a821e2e719b55505c6388675e3fce2720f0e3b3
Top <weerapat.thawatchoketawee@gmail.com>
2018-02-25 22:57:26 +0700

    Test Login Facebook

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 import express from 'express';
+import session from 'express-session';
 import cors from 'cors';
 import path from 'path';
 import favicon from 'serve-favicon';
 import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import passport from 'passport';
+import { Strategy as FacebookStrategy } from 'passport-facebook';
 import { web, api } from './app/routes';
 import { codeStatus } from './app/controllers/Controller';
 
+const CLIENT_ID = 'client id จาก facebook';
+const CLIENT_SECRET = 'client secret จาก facebook';
+
 // App Express
 const app = express();
+
+// Set passport
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+passport.use(new FacebookStrategy(
+  {
+    clientID: 1028852790492705,
+    clientSecret: '0dd2509758802bdf54a3d2b034dc07b8',
+    callbackURL: 'http://localhost:8000/auth/facebook/callback',
+  },
+  (accessToken, refreshToken, profile, done) => {
+    // ส่วนนี้จะเอาข้อมูลที่ได้จาก facebook ไปทำอะไรต่อก็ได้
+    done(null, profile);
+  },
+));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,11 +46,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/', web);
 app.use('/api', api);
+
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['read_stream', 'publish_actions'] }));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/profile', failureRedirect: '/' }));
+app.get('/profile', (req, res) => {
+  res.json(req.user);
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
